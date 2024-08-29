fbd0c146fb661428bf2e64d5c9b410bb1c80d758
TechyCSR <csr.info.in@gmail.com>
2024-08-29 18:52:29 +0530

    Day1 Submission

---
+++ Day 1/Ques-01.cpp
@@ -0,0 +1,72 @@
+// #include <iostream>
+// #include <string>
+
+// using namespace std;
+
+// int main() {
+//     // Declare variables for the input strings
+//     string firstString, secondString;
+
+//     // Take input from the user
+//     cout << "Enter the first string: ";
+//     cin >> firstString;
+    
+//     cout << "Enter the second string: ";
+//     cin >> secondString;
+
+//     // Concatenate the strings
+//     string concatenatedString = firstString + secondString;
+
+//     // Output the concatenated string
+//     cout << "Concatenated string: " << concatenatedString << endl;
+
+//     return 0;
+// }
+
+
+// #include<iostream>
+
+// using namespace std;
+
+// int main(){
+//     int sub1,sub2,sub3,sub4,sub5;
+//     cout<< "Enter First Subject: ";
+//     cin>>sub1;
+//     cout<<" Enter Second Subject: ";
+//         cin>>sub2;
+
+//     cout<<"Enter Third Subject: ";
+//         cin>>sub3;
+
+//     cout<<"Enter Fourth Subject: ";
+//         cin>>sub4;
+
+//     cout<<"Enter Fifth Subject: ";
+//         cin>>sub5;
+
+//     int avg=(sub1+sub2+sub3+sub4+sub5)/5;
+
+//     cout<<"The Average of Subjects is "<<avg;
+
+//     return 0;
+
+
+
+
+// }
+
+
+
+
+// #include<iostream>
+
+
+// using namespace std;
+
+// int main(){
+//     int num1;
+//     cout<<"Enter a  Number: ";
+//     cin>>num1;
+//     cout<<"Square of Number : "<<(num1*num1);
+//     return 0;
+// }
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# CipherSchoolAssigments
-Assignments Submission Repo :)
+# __CipherSchool C++ Assignments__
+
+This repository contains my daily C++ assignments for CipherSchool. Each day is organized into its own folder, and within each folder, you'll find C++ files corresponding to the questions assigned for that day.
+
+__Course Instructor:__ Ashis Sir  
+__Profile Link:__ [Your LinkedIn Profile](#)
+
+---
+
+## __Repository Structure__
+
+Each day of the course has its own folder, containing solutions to the assigned questions. The structure is as follows:
+
+- **Day 1/**
+  - `Ques-01.cpp` : Solution for Question 1
+  - `Ques-02.cpp` : Solution for Question 2
+  - ...
+  
+- **Day 2/**
+  - `Ques-01.cpp` : Solution for Question 1
+  - `Ques-02.cpp` : Solution for Question 2
+  - ...
+
+- **Day 3/**
+  - `Ques-01.cpp` : Solution for Question 1
+  - `Ques-02.cpp` : Solution for Question 2
+  - ...
+
+...and so on.
+
+---
+
+## __How to Use__
+
+1. Navigate to the folder for the specific day you're interested in.
+2. Open the relevant `.cpp` file to view or edit the solution to the question.
+3. Compile and run the code to test the solution.
+
+---
+
+__Note:__  
+These assignments are part of my learning journey in C++ under the guidance of Ashis Sir. Each solution is crafted with a focus on improving problem-solving skills using C++.
+
