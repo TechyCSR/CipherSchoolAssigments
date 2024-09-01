17f58728b0bd8b445def985a9b1fd8ee06316354
CHANDAN SINGH <90786270+TechyCSR@users.noreply.github.com>
2024-09-01 09:31:30 +0530

    Update Ques-02.cpp

--- Day 1/Ques-02.cpp
+++ Day 1/Ques-02.cpp
@@ -4,20 +4,16 @@
 using namespace std;
 
 int main() {
-    // Declare variables for the input strings
     string firstString, secondString;
 
-    // Take input from the user
     cout << "Enter the first string: ";
     cin >> firstString;
     
     cout << "Enter the second string: ";
     cin >> secondString;
 
-    // Concatenate the strings
     string concatenatedString = firstString + secondString;
 
-    // Output the concatenated string
     cout << "Concatenated string: " << concatenatedString << endl;
 
     return 0;
